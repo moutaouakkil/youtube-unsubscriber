b99e57f573afa6a52a6fef42445190b0fe2fdab5
Othmane Moutaouakkil <othmane.m.mt2507@gmail.com>
2019-08-01 11:22:04 +0100

    Create youtube-unsubscriber.js

---
+++ youtube-unsubscriber.js
@@ -0,0 +1,33 @@
+var i = 0;
+
+var myVar = setInterval(myTimer, 3000);
+
+function myTimer() {
+
+    var els = document.getElementById("grid-container").getElementsByClassName("ytd-expanded-shelf-contents-renderer");
+
+    if (i < els.length) {
+
+        els[i].querySelector("[aria-label^='Unsubscribe from']").click();
+
+        setTimeout(function () {
+
+            var unSubBtn = document.getElementById("confirm-button").click();
+
+        }, 2000);
+
+        setTimeout(function () {
+
+            els[i].parentNode.removeChild(els[i]);
+
+        }, 2000);
+
+    }
+
+    i++;
+
+    console.log(i + " Channels Unsubscribed\n");
+
+    console.log(els.length + " remaining");
+
+}
