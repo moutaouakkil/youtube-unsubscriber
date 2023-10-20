d83b450d009124167ccf8823942a3edcb89e94b4
Alexis Toby <alexistobyjr@gmail.com>
2023-10-20 13:30:27 +0200

    Update youtube-unsubscriber.js
    Updated the click event logic of the "Unsubscribe"

--- youtube-unsubscriber.js
+++ youtube-unsubscriber.js
@@ -10,13 +10,52 @@ function myTimer() {
 
         els[i].querySelector("[aria-label^='Unsubscribe from']").click();
 
-        setTimeout(function () {
+        setTimeout(function() {
 
-            var unSubBtn = document.getElementById("confirm-button").click();
+            var i = 0;
+
+            var myVar = setInterval(myTimer, 3000);
+
+            function myTimer() {
+
+                var els = document.getElementById("grid-container").getElementsByClassName("ytd-expanded-shelf-contents-renderer");
+
+                if (i < els.length) {
+
+                    els[i].querySelector("[aria-label^='Unsubscribe from']").click();
+
+                    setTimeout(function() {
+
+                        var confirmButton = document.getElementById("confirm-button");
+                        if (confirmButton) {
+                            var button = confirmButton.querySelector("yt-button-shape button");
+                            if (button) {
+                                button.click();
+                            }
+                        }
+
+
+                    }, 2000);
+
+                    setTimeout(function() {
+
+                        els[i].parentNode.removeChild(els[i]);
+
+                    }, 2000);
+
+                }
+
+                i++;
+
+                console.log(i + " Channels Unsubscribed\n");
+
+                console.log(els.length + " remaining");
+
+            }
 
         }, 2000);
 
-        setTimeout(function () {
+        setTimeout(function() {
 
             els[i].parentNode.removeChild(els[i]);
 
